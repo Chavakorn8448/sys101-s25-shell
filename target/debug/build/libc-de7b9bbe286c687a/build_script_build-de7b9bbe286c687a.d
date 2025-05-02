002378a3cb54ea7332778b4727639edb242debfd
Chavakorn <arunkunarax.c.8448@gmail.com>
2025-05-02 10:38:52 +0700

    target deleted

--- target/debug/build/libc-de7b9bbe286c687a/build_script_build-de7b9bbe286c687a.d
+++
@@ -1,5 +0,0 @@
-/home/traffy/test/sys101-s25-shell/target/debug/build/libc-de7b9bbe286c687a/build_script_build-de7b9bbe286c687a: /home/traffy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.171/build.rs
-
-/home/traffy/test/sys101-s25-shell/target/debug/build/libc-de7b9bbe286c687a/build_script_build-de7b9bbe286c687a.d: /home/traffy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.171/build.rs
-
-/home/traffy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.171/build.rs:
--- target/debug/vssh.d
+++
@@ -1,1 +0,0 @@
-/home/traffy/test/sys101-s25-shell/target/debug/vssh: /home/traffy/test/sys101-s25-shell/src/bin/vssh.rs
